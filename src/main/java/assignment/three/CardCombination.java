2f8c98e327d908dd855b9365c5708fc78bc023f3
Gareth Sears <garethsears@gmail.com>
2019-11-21 16:39:47 +0000

    extract maximum finder and add to remove method

--- src/main/java/assignment/three/CardCombination.java
+++ src/main/java/assignment/three/CardCombination.java
@@ -13,15 +13,10 @@ public class CardCombination {
     public CardCombination() {
     }
 
-    public CardCombination(Map<Card, Integer> cardCountMap) {
+    // This is only used to generate new card combos following filter operations
+    private CardCombination(Map<Card, Integer> cardCountMap) {
         this.cardCountMap = cardCountMap;
-
-        // Find the new maximum int inside the cardCountMap
-        maxCardCount = cardCountMap.entrySet().stream()
-                // Get the counts of each card
-                .mapToInt(x -> x.getValue())
-                // Return the max or 0 if none exists
-                .max().orElse(0);
+        setMaximum(); // Traverse the map and set the new maximum card count.
     }
 
     public void add(Card card) {
@@ -38,7 +33,7 @@ public class CardCombination {
         } else {
             // Add the card to the cache and set its count to one.
             cardCountMap.put(card, 1);
-            // Set the max count to 0 if there's no other value...
+            // Set the max count to 1 if its the first element...
             if (maxCardCount == 0) {
                 maxCardCount = 1;
             }
@@ -54,7 +49,13 @@ public class CardCombination {
     }
 
     public void remove(Card card) {
-        cardCountMap.remove(card);
+        if (cardCountMap.get(card) == maxCardCount) {
+            cardCountMap.remove(card);
+            // Maybe we deleted the card with the maximum count? Reset it to be sure.
+            setMaximum();
+        } else {
+            cardCountMap.remove(card);
+        }
     }
 
     public void clear() {
@@ -70,27 +71,42 @@ public class CardCombination {
         return cardCountMap;
     }
 
+    private void setMaximum() {
+        // Find the new maximum card count inside the cardCountMap
+        maxCardCount = cardCountMap.entrySet().stream()
+                // Get the counts of each card
+                .mapToInt(x -> x.getValue())
+                // Return the max or 0 if none exists
+                .max().orElse(0);
+    }
+
     /*
-     * 
-     * FILTER FUNCTIONS ----------------
+     * --------------- FILTER FUNCTIONS ----------------
      * 
      * Return card combinations based on a filter function. Useful for outputing 'winning'
-     * combinations, or just the jokers.
+     * combinations, or just the jokers, but allowing easy modification for future machines.
      * 
-     * CardCombination test = new CardCombination(); test.add(Card.JOKER); test.add(Card.JOKER);
-     * test.add(Card.ACE); test.add(Card.QUEEN);
+     * SEE THE FOLLOWING FOR EXAMPLE USE:
+     * 
+     * CardCombination test = new CardCombination(); 
+     * test.add(Card.JOKER); 
+     * test.add(Card.JOKER);
+     * test.add(Card.ACE); 
+     * test.add(Card.QUEEN);
      * 
      * System.out.println(test); // Map: {Queen=1, Ace=1, Joker=2} MaxCount: 2
      * 
-     * // Return cards where there are two of a kind CardCombination filteredByCount =
-     * test.filterByCount(x -> x == 1); // Map: {Queen=1, Ace=1} MaxCount: 1
+     * // Return only the single cards in the spinners.
+     * CardCombination filteredByCount = test.filterByCount(x -> x == 1); 
+     * // Map: {Queen=1, Ace=1} MaxCount: 1
      * 
-     * // Return cards which have the maximum value CardCombination filteredByCount =
-     * test.filterByCount(x -> x == test.getMaxCardCount()); // Map: {Joker=2} MaxCount: 2
+     * // Return cards which appear the most in the spinners.
+     * CardCombination filteredByCount = test.filterByCount(x -> x == test.getMaxCardCount()); 
+     * // Map: {Joker=2} MaxCount: 2
      * 
-     * // Return cards which are either Jokers or Aces CardCombination filteredByCard =
-     * test.filterByCard(x -> x == Card.JOKER || x == Card.ACE); // Map: {Ace=1, Joker=2} MaxCount:
-     * 2
+     * // Return cards which are either Jokers or Aces.
+     * CardCombination filteredByCard = test.filterByCard(x -> x == Card.JOKER || x == Card.ACE); 
+     * // Map: {Ace=1, Joker=2} MaxCount: 2
      * 
      */
     public CardCombination filterByCount(Predicate<? super Integer> filterFunction) {
@@ -100,16 +116,18 @@ public class CardCombination {
                 // Collect to a map
                 .collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
 
+        // Immutable
         return new CardCombination(filteredMap);
     }
 
     public CardCombination filterByCard(Predicate<? super Card> filterFunction) {
         Map<Card, Integer> filteredMap = cardCountMap.entrySet().stream()
-                // Filter the stream by the filterFunction acting on Cards
+                // Filter the stream by the filterFunction acting on cards
                 .filter(x -> filterFunction.test(x.getKey()))
                 // Collect to a map
                 .collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
 
+        // Immutable
         return new CardCombination(filteredMap);
     }
 
