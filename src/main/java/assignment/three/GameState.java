193648141229dec746f132df4e6e5a4cf46fdf0a
Gareth Sears <garethsears@gmail.com>
2019-11-22 22:51:42 +0000

    Update formatting

--- src/main/java/assignment/three/GameState.java
+++ src/main/java/assignment/three/GameState.java
@@ -6,6 +6,7 @@ package assignment.three;
  * This enum represents the different states a fruit machine can have. Using an enum adds clarity to
  * the source code and allows for easy modifications.
  */
+
 public enum GameState {
     WON, LOST, PLAY;
 }
