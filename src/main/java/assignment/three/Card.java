a5e6b73d5c8731d65ccb8bb5595b123bbd0a583e
Gareth Sears <garethsears@gmail.com>
2019-11-30 22:55:34 +0000

    comment card class

--- src/main/java/assignment/three/Card.java
+++ src/main/java/assignment/three/Card.java
@@ -1,8 +1,17 @@
 package assignment.three;
 
+/**
+ * Card.java - Gareth Sears | 2493194
+ * 
+ * A class which represents a 'card' on a fruit machine spinner. This class was created so that
+ * different machines can use different card sets. Originally an enum was used, but this was too
+ * restrictive for future changes.
+ */
+
 public class Card {
     private String name;
 
+    // Ensure lowercase
     Card(String name) {
         this.name = name.toLowerCase();
     }
@@ -11,6 +20,7 @@ public class Card {
         return name;
     }
 
+    // Override hashCode so that it can be stored in the hashmap used by CardCounts
     @Override
     public int hashCode() {
         return name.hashCode();
@@ -22,6 +32,7 @@ public class Card {
         return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
     }
 
+    // Compare cards based on their name.
     @Override
     public boolean equals(Object obj) {
         if (obj != null && obj instanceof Card) {
