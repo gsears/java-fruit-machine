f7794955ad5878cd29d348a517f947b8d14996b1
Gareth Sears <garethsears@gmail.com>
2019-12-11 23:10:21 +0000

    remove string alter in constructor, update comments

--- src/main/java/assignment/three/Card.java
+++ src/main/java/assignment/three/Card.java
@@ -4,16 +4,14 @@ package assignment.three;
  * Card.java - Gareth Sears | 2493194
  * 
  * A class which represents a 'card' on a fruit machine spinner. This class was created so that
- * different machines can use different card sets. Originally an enum was used, but this was too
- * restrictive for future changes.
+ * different machines can use different card sets.
  */
 
 public class Card {
     private String name;
 
-    // Ensure lowercase
     Card(String name) {
-        this.name = name.toLowerCase();
+        this.name = name;
     }
 
     public String getName() {
@@ -35,13 +33,13 @@ public class Card {
     // Compare cards based on their name.
     @Override
     public boolean equals(Object obj) {
+        // If the object isn't null and is a card...
         if (obj != null && obj instanceof Card) {
+            // ...check if the name Strings are equal.
             Card card = (Card) obj;
             return name.equals(card.getName());
         } else {
             return false;
         }
-
     }
-
 }
