68601e23ab15356ee641f3de08665ed353d6df74
Gareth Sears <garethsears@gmail.com>
2019-12-11 23:17:13 +0000

    update comments and remove unneccesary gridbag constraints

--- src/main/java/assignment/three/SpinnerView.java
+++ src/main/java/assignment/three/SpinnerView.java
@@ -1,7 +1,6 @@
 package assignment.three;
 
 import java.awt.Color;
-import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -10,35 +9,30 @@ import javax.swing.JPanel;
 /**
  * SpinnerView.java | Gareth Sears - 2493194
  * 
- * Encapsulates the 'card' view, so many cards can be made easily.
- * 
+ * A JPanel which encapsulates the card view.
  */
 
 @SuppressWarnings("serial")
 public class SpinnerView extends JPanel {
 
-    JLabel cardTextLabel;
+    private JLabel cardTextLabel;
 
+    // CONSTRUCTOR
     public SpinnerView() {
         this(""); // Initialise with no text if none is provided.
     }
 
+    // CONSTRUCTOR
     public SpinnerView(String initialText) {
 
-        setLayout(new GridBagLayout());
+        setLayout(new GridBagLayout()); // Gridbag used to centralise text.
         setBackground(Color.YELLOW); // Let's burn some retinas.
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         cardTextLabel = new JLabel();
-        setText(initialText);
+        cardTextLabel.setText(initialText);
 
-        // GridBagLayout used to center text nicely.
-        GridBagConstraints gc = new GridBagConstraints();
-        gc.gridx = 0;
-        gc.gridy = 0;
-        gc.anchor = GridBagConstraints.CENTER;
-
-        add(cardTextLabel, gc);
+        add(cardTextLabel);
     }
 
     public void setText(String text) {
