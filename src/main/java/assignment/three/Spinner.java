a2cd3c6b66afaa39864bd96f333f1b44f31a20c8
Gareth Sears <garethsears@gmail.com>
2019-12-11 23:16:42 +0000

    update method names and comments

--- src/main/java/assignment/three/Spinner.java
+++ src/main/java/assignment/three/Spinner.java
@@ -5,27 +5,25 @@ import java.util.Random;
 /**
  * Spinner.java | Gareth Sears - 2493194
  * 
- * A spinner returns a random enum from the Card class.
+ * A spinner returns a random card from the set passed to the constructor.
  */
 
 public class Spinner {
 
-    // Static to avoid duplicating across spinners...
+    // Static to avoid duplicating across spinners.
     private static Random randomNumberGenerator = new Random();
-    private Card[] cardArray;
-    private Card card;
+    private Card[] cardSet; // The card set from which cards are selected.
+    private Card card; // The current card.
 
-    public Spinner(Card[] cardArray) {
-        this.cardArray = cardArray; // pass by ref.
+    public Spinner(Card[] cardSet) {
+        this.cardSet = cardSet;
         spin(); // Initialise with a random card.
     }
 
     public Card spin() {
-
-        int randomIndex = randomNumberGenerator.nextInt(cardArray.length); // Get random index
-        card = cardArray[randomIndex]; // Get random card and set the attribute
-
-        return card; // Return the card for convenience (can be ignored)
+        int randomIndex = randomNumberGenerator.nextInt(cardSet.length); // Get random index
+        card = cardSet[randomIndex]; // Pick the card from the card set.
+        return card; // For convenience, can be ignored.
     }
 
     public Card getCard() {
